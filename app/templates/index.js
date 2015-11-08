85dab520489814b241a61be69da0670d5fd38833
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-11-08 14:45:42 +0800

    fix opts null bug

--- app/templates/index.js
+++ app/templates/index.js
@@ -6,7 +6,7 @@ var through = require('through2');
 var defaultOpts = {};
 
 module.exports = function(opts) {
-  opts = _.defaults(opts, defaultOpts);
+  opts = _.defaults(opts || {}, defaultOpts);
   return through.obj(function(file, encoding, callback) {
     // your logic here
     callback(null, file);
